94caff40e278869ade6bded6f1d648abceb2ae0f
drob1843 <94759314+drob1843@users.noreply.github.com>
2024-10-30 21:50:38 -0700

    Complete

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', () => {
+    const form = document.getElementById('empForm')
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const id = document.getElementById('id').value
+        const name = document.getElementById('name').value
+        const ext = document.getElementById('ext').value
+        const email = document.getElementById('email').value
+        const department = document.getElementById('department').value
+
+        console.log(`ID: ${id}`)
+        console.log(`Name: ${name}`)
+        console.log(`Extension: ${ext}`)
+        console.log(`Email: ${email}`)
+        console.log(`Department ${department}`)
+    })
+})
